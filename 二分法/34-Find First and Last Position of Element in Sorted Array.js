f5100b03061930f9114e76ef937811e4d3a9f931
King23WL <72429957+King23WL@users.noreply.github.com>
2022-08-20 22:53:06 -0700

    704

---
+++ 二分法/34-Find First and Last Position of Element in Sorted Array.js
@@ -0,0 +1,58 @@
+// Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
+
+// If target is not found in the array, return [-1, -1].
+
+// You must write an algorithm with O(log n) runtime complexity.
+
+// Example 1:
+
+// Input: nums = [5,7,7,8,8,10], target = 8
+// Output: [3,4]
+// Example 2:
+
+// Input: nums = [5,7,7,8,8,10], target = 6
+// Output: [-1,-1]
+// Example 3:
+
+// Input: nums = [], target = 0
+// Output: [-1,-1]
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange = function (arr, key) {
+  result = [-1, -1];
+  result[0] = binary_search(arr, key, false);
+  if (result[0] !== -1) {
+    // no need to search, if 'key' is not present in the input array
+    result[1] = binary_search(arr, key, true);
+  }
+
+  return result;
+};
+
+function binary_search(arr, key, findMaxIndex) {
+  let keyIndex = -1;
+  let start = 0;
+  let end = arr.length - 1;
+  while (start <= end) {
+    mid = Math.floor(start + (end - start) / 2);
+    if (key < arr[mid]) {
+      end = mid - 1;
+    } else if (key > arr[mid]) {
+      start = mid + 1;
+    } else {
+      // key === arr[mid]
+      keyIndex = mid;
+      if (findMaxIndex) {
+        start = mid + 1; // search ahead to find the last index of 'key'
+      } else {
+        end = mid - 1; // search behind to find the first index of 'key'
+      }
+    }
+  }
+
+  return keyIndex;
+}
--- 二分法/704-BinarySearch.js
+++ 二分法/704-BinarySearch.js
@@ -18,17 +18,18 @@
  * @param {number} target
  * @return {number}
  */
- var search = function(nums, target) {
-    let l = 0, r = nums.length - 1;
-      
-    while (l <= r) {
-          // calc middle index
-          const mid = Math.floor((r + l) / 2);
-          if (nums[mid] === target) return mid;
-          if (nums[mid] < target) l = mid + 1;
-          else r = mid - 1;
-      }
-      return -1; 
-  };
-  
-  //O logn
+var search = function (nums, target) {
+  let l = 0,
+    r = nums.length - 1;
+
+  while (l <= r) {
+    // calc middle index
+    const mid = Math.floor((r + l) / 2);
+    if (nums[mid] === target) return mid;
+    if (nums[mid] < target) l = mid + 1;
+    else r = mid - 1;
+  }
+  return -1;
+};
+
+//O logn
