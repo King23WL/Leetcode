e65ac05f3ffe2993e2c4cf98f846e5eb34f15412
King23WL <72429957+King23WL@users.noreply.github.com>
2022-08-23 13:55:28 -0700

    658

---
+++ 二分法/658-Find K Closet Elements.js
@@ -0,0 +1,39 @@
+// Given a sorted integer array arr, two integers k and x, return the k closest integers to x in the array. The result should also be sorted in ascending order.
+
+// An integer a is closer to x than an integer b if:
+
+// |a - x| < |b - x|, or
+// |a - x| == |b - x| and a < b
+
+// Example 1:
+
+// Input: arr = [1,2,3,4,5], k = 4, x = 3
+// Output: [1,2,3,4]
+// Example 2:
+
+// Input: arr = [1,2,3,4,5], k = 4, x = -1
+// Output: [1,2,3,4]
+
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @param {number} x
+ * @return {number[]}
+ */
+var findClosestElements = function (arr, k, x) {
+  let left = 0;
+  let right = arr.length - k;
+
+  while (left < right) {
+    let mid = left + Math.floor((right - left) / 2);
+
+    if (x - arr[mid] > arr[mid + k] - x) {
+      left = mid + 1;
+    } else {
+      right = mid;
+    }
+  }
+  return arr.slice(left, left + k);
+};
+
+//O(log(N−k)+k)
